581110947ed747d1146d9d7396f0f5454bac8fed
YAGO ANDRADE MOLINA <yago.molina@portalsesisp.org.br>
2026-03-18 09:59:53 -0300

    Quarto Commit

--- command_runner/lib/command_runner.dart
+++ command_runner/lib/command_runner.dart
@@ -1,8 +1,5 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library;
 
+export 'src/args.dart';
 export 'src/command_runner_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/help_command.dart';
